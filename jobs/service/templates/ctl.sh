edd9983fe40fc5ab9b96acee6d7ae36e65b51d25
Pivotal <pivotal@dx224.tor.pivotallabs.com>
2016-03-04 16:33:35 -0500

    hopefully links work :

--- jobs/service/templates/ctl.sh
+++ jobs/service/templates/ctl.sh
@@ -13,6 +13,8 @@ exec 2>> $LOG_DIR/$JOB_NAME.stderr.log
 
 source /var/vcap/packages/server/pid_utils.sh
 
+cp /var/vcap/jobs/server/config.yml /tmp
+
 case $1 in
 
   start)
@@ -20,7 +22,7 @@ case $1 in
 
     echo $$ > $PIDFILE
 
-    exec chpst -u vcap:vcap /var/vcap/packages/service/simple_server
+    exec chpst -u vcap:vcap /var/vcap/packages/service/simple_server 8081
 
     ;;
 
